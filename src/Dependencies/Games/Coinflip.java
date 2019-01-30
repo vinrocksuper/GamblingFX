16ad5bdbf87068a0d5de44762469c88c76d880fb
BT_1N3_02 <vli8558@bths.edu>
2019-01-30 10:08:04 -0500

    We're screwed lul

--- src/Dependencies/Games/Coinflip.java
+++ src/Dependencies/Games/Coinflip.java
@@ -1,4 +1,28 @@
 package Dependencies.Games;
 
+import java.awt.event.ActionEvent;
+
 public class Coinflip {
+
+/**
+ * To be coded later.
+    public boolean bet(int b, ActionEvent choice)
+    {
+        return true;
+    }
+ **/
+    public boolean roll(boolean head)
+    {
+        double roll = Math.random();
+        if(head && roll>.5)
+        {
+            return true;
+        }
+        if(!head && roll<.5)
+        {
+            return true;
+        }
+        return false;
+    }
+
 }
