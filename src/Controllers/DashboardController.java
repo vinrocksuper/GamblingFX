45fd34543b831fa419a7baa626b6b2b4723f9c6e
Afaq Anwar <afaqanwar.se@outlook.com>
2019-01-31 22:41:50 -0500

    Setup basic FXML files for all tabs.

--- src/Controllers/DashboardController.java
+++ src/Controllers/DashboardController.java
@@ -28,12 +28,15 @@ public class DashboardController {
     private void buildTabs() throws IOException {
         Tab jackpotTab = new Tab("Jackpot");
         tabPane.getTabs().add(jackpotTab);
+        jackpotTab.setContent(FXMLLoader.load(getClass().getResource("/Resources/FXML/JackpotGame.fxml")));
         Tab rouletteTab = new Tab("Roulette");
         tabPane.getTabs().add(rouletteTab);
+        rouletteTab.setContent(FXMLLoader.load(getClass().getResource("/Resources/FXML/RouletteGame.fxml")));
         Tab crashTab = new Tab("Crash");
         crashTab.setContent(FXMLLoader.load(getClass().getResource("/Resources/FXML/CrashGame.fxml")));
         tabPane.getTabs().add(crashTab);
         Tab coinflipTab = new Tab("Coin Flip");
         tabPane.getTabs().add(coinflipTab);
+        coinflipTab.setContent(FXMLLoader.load(getClass().getResource("/Resources/FXML/CoinflipGame.fxml")));
     }
 }
