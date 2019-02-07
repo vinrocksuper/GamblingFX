ae74ed39fe941cecc3b23c39ccb28aed546bef60
Afaq Anwar <afaqanwar.se@outlook.com>
2019-02-07 16:09:38 -0500

    Updated System Critical Files.
    Signed-off-by: Afaq Anwar <afaqanwar.se@outlook.com>

--- src/Controllers/CrashGameController.java
+++ src/Controllers/CrashGameController.java
@@ -1,30 +1,41 @@
 package Controllers;
 
 import Dependencies.Games.Crash;
+import Dependencies.Systems.User;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDialog;
 import com.jfoenix.controls.JFXTextField;
+import javafx.animation.AnimationTimer;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.chart.LineChart;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
+import sun.rmi.runtime.Log;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Controller for the Crash Game Scene.
+ * TO DO - Multi Colored Circles, Infinite Game, Circle Animations
  * @Authors Afaq Anwar & Wai Hin Leung
- * @Version 01/31/2019
+ * @Version 02/07/2019
  */
 public class CrashGameController {
     @FXML private Pane pane;
-    @FXML private LineChart graph;
-    @FXML private VBox playerList;
+    @FXML private Circle circle;
+    @FXML private VBox playerPane;
     @FXML private JFXTextField amountField;
     @FXML private JFXButton playButton;
     @FXML private JFXButton clearButton;
     @FXML private StackPane stackPane;
+    @FXML private Label timer;
+    @FXML private Label textMultiplier;
 
     private Crash crashGame;
 
@@ -32,8 +43,55 @@ public class CrashGameController {
      * Runs on Scene load.
      */
     public void initialize() {
-        crashGame = new Crash();
-        crashGame.addCurrentPlayer(LoginController.currentUser);
+        crashGame = new Crash(LoginController.currentUser);
+        this.startPregameTimer();
+    }
+
+    /**
+     * Initializes the 10 second timer before the Crash game starts.
+     */
+    private void startPregameTimer() {
+        Timer pregameTimer = new Timer();
+        TimerTask countdown = new TimerTask() {
+            int currSeconds = 10;
+            @Override
+            public void run() {
+                if (currSeconds > 0) {
+                    Platform.runLater(() -> timer.setText("Starting in: " + currSeconds + " Seconds"));
+                    currSeconds--;
+                } else {
+                    Platform.runLater(() -> disableControls(false));
+                    Platform.runLater(() -> timer.setVisible(false));
+                    Platform.runLater(() -> disableFunctionalControls(true));
+                    Platform.runLater(() -> animateGame());
+                    this.cancel();
+                }
+            }
+        };
+        pregameTimer.schedule(countdown, 1000, 1000);
+    }
+
+    /**
+     * Animates the game using critical functions.
+     */
+    private void animateGame() {
+        AnimationTimer gameTimer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                if (crashGame.isGameRunning()) {
+                    crashGame.setCurrentMultiplier(crashGame.getCurrentMultiplier() + ((crashGame.getBustingMultiplier() / 5.7) * 0.05));
+                    crashGame.updatePlayerMultipliers();
+                    textMultiplier.setText(Double.toString((double) Math.round(crashGame.getCurrentMultiplier() * 100) / 100));
+                    circle.setRadius(circle.getRadius() + ((crashGame.getBustingMultiplier() / 5.7) * 0.05));
+                    updatePlayerList();
+                } else {
+                    crashGame.setLosingPlayers();
+                    updatePlayerList();
+                    this.stop();
+                }
+            }
+        };
+        gameTimer.start();
     }
 
     /**
@@ -42,13 +100,19 @@ public class CrashGameController {
      */
     @FXML
     private void handleButtons(ActionEvent actionEvent) {
-        if (actionEvent.getSource() == playButton && crashGame.userManager.getStatusOfUser(LoginController.currentUser).equals("Playing")) {
+        if (actionEvent.getSource() == playButton && crashGame.userManager.getStatusOfUser(LoginController.currentUser).equals("Playing") && crashGame.isGameRunning()) {
             crashGame.userManager.setStatusOfUser(LoginController.currentUser, "Won");
+            this.displayWinningAlert();
+            crashGame.updatePlayersBalance();
+            System.out.println(LoginController.currentUser.getBalance());
             playButton.setText("Play");
         } else if (actionEvent.getSource() == playButton) {
-            if (validateAmount()) {
+            if (validateBet()) {
                 crashGame.userManager.setStatusOfUser(LoginController.currentUser, "Playing");
+                crashGame.placeBet(Integer.parseInt(amountField.getText()), LoginController.currentUser);
                 playButton.setText("Withdraw");
+                this.updatePlayerList();
+                this.disableControls(true);
             } else {
                 this.displayAlert();
             }
@@ -59,12 +123,14 @@ public class CrashGameController {
     }
 
     /**
-     * Makes sure the betting amount entered is a hole number that is less than 1,000,000,000.
+     * Makes sure the betting amount entered is a whole number that is less than 1,000,000,000.
      * @return True if it meets the requirements, false otherwise.
      */
-    private boolean validateAmount() {
-      return (amountField.getText().matches("[0-9]") && amountField.getText().length() < 10);
+    private boolean validateBet() {
+        return (amountField.getText().length() < 10 && amountField.getText().length() > 0 && crashGame.userManager.validateAmount(LoginController.currentUser, Integer.parseInt(amountField.getText())));
     }
+
+
 
     /**
      * Displays alerts based on the user error.
@@ -78,6 +144,61 @@ public class CrashGameController {
             JFXDialog dialog = new JFXDialog();
             dialog.setContent(new Label("Cannot place a bet over 1,000,000,000!"));
             dialog.show(stackPane);
+        } else if (amountField.getText().length() == 0) {
+            JFXDialog dialog = new JFXDialog();
+            dialog.setContent(new Label("Please place a valid bet amount."));
+            dialog.show(stackPane);
+        } else if (crashGame.userManager.validateAmount(LoginController.currentUser, Integer.parseInt(amountField.getText()))) {
+            JFXDialog dialog = new JFXDialog();
+            dialog.setContent(new Label("Insufficient Funds!"));
+            dialog.show(stackPane);
+        }
+    }
+
+
+    /**
+     * Displays a "winning" JFXDialog if the User has won the game.
+     */
+    private void displayWinningAlert() {
+        JFXDialog dialog = new JFXDialog();
+        dialog.setContent(new Label("You have won " + Math.floor(crashGame.getPlayerBet(LoginController.currentUser) * crashGame.getPlayerMultiplier(LoginController.currentUser))));
+        dialog.show(stackPane);
+    }
+
+    /**
+     * Disables amount field, and clear button.
+     * @param bool
+     */
+    private void disableFunctionalControls(boolean bool) {
+        amountField.setDisable(true);
+        clearButton.setDisable(bool);
+    }
+
+    /**
+     * Disables controls.
+     * @param bool True if all controls are to be disabled, false otherwise.
+     */
+    private void disableControls(boolean bool) {
+        playButton.setDisable(bool);
+        clearButton.setDisable(bool);
+        amountField.setDisable(bool);
+    }
+
+    /**
+     * Updates the List of Players with Labels inside of the playerPane VBOX.
+     */
+    private void updatePlayerList() {
+        playerPane.getChildren().clear();
+        for (User user : crashGame.userManager.getCurrentActiveUsers()) {
+            Label userLabel = new Label();
+            if (!crashGame.userManager.getStatusOfUsers().containsKey(user)) {
+                userLabel.setText(user.getFirstName().toUpperCase() + " [IDLE]");
+            } else if (crashGame.userManager.getStatusOfUser(user).equals("Lost")) {
+                userLabel.setText(user.getFirstName().toUpperCase() + " [" + crashGame.userManager.getStatusOfUser(user) + "] BET: " + crashGame.getPlayerBet(user) + " @ X.XX");
+            } else {
+                userLabel.setText(user.getFirstName().toUpperCase() + " [" + crashGame.userManager.getStatusOfUser(user) + "] BET: " + crashGame.getPlayerBet(user) + " @ " +(double) Math.round(crashGame.getPlayerMultiplier(user) * 100) / 100);
+            }
+            playerPane.getChildren().add(userLabel);
         }
     }
 }
--- src/Dependencies/Games/Crash.java
+++ src/Dependencies/Games/Crash.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 /**
  * Crash Game
  * @Authors Afaq Anwar & Wai Hin Leung
- * @Version 01/31/2019
+ * @Version 02/07/2019
  */
 public class Crash extends GamblingGame {
     // The current multiplier of the Game.
@@ -17,25 +17,52 @@ public class Crash extends GamblingGame {
     private double bustingMultiplier;
     // HashMap that represents each player along with their multiplier.
     private HashMap<User, Double> currentPlayerMultipliers;
+    private HashMap<User, Integer> currentPlayerBets;
+    private boolean gameRunning;
 
     /**
      * Main Constructor.
      */
     public Crash() {
-        this.currentMultiplier = generateRandomMultiplier();
+        this.currentMultiplier = 0;
         this.bustingMultiplier = generateRandomMultiplier();
-        currentPlayerMultipliers = generateInitialPlayerMap(userManager.getCurrentActiveUsers());
+        this.currentPlayerMultipliers = generateInitialPlayerMap(userManager.getCurrentActiveUsers());
+        this.currentPlayerBets = generateInitialBetMap(userManager.getCurrentActiveUsers());
+        this.gameRunning = false;
+    }
+
+    /**
+     * Secondary Constructor
+     * @param user Any User.
+     */
+    public Crash(User user) {
+        this.addCurrentPlayer(user);
+        this.currentMultiplier = 0;
+        this.bustingMultiplier = generateRandomMultiplier();
+        this.currentPlayerMultipliers = generateInitialPlayerMap(userManager.getCurrentActiveUsers());
+        this.currentPlayerBets = generateInitialBetMap(userManager.getCurrentActiveUsers());
+        this.gameRunning = false;
     }
 
     // Getters & Setters for some private fields.
     public double getCurrentMultiplier() { return this.currentMultiplier; }
     public void setCurrentMultiplier(double currentMultiplier) { this.currentMultiplier = currentMultiplier; }
     public double getBustingMultiplier() { return this.bustingMultiplier; }
+    public boolean isGameRunning() {
+        this.updateGameStatus();
+        return this.gameRunning;
+    }
+    public Integer getPlayerBet(User user) { return this.currentPlayerBets.get(user); }
+    public Double getPlayerMultiplier(User user) { return this.currentPlayerMultipliers.get(user); }
 
     /**
      * @return Random Double between 0 - 10,000.
      */
-    private double generateRandomMultiplier() { return (Math.random() * 10000); }
+    private double generateRandomMultiplier() {
+        double randomChance = (Math.random() * 0.1);
+        System.out.println(randomChance);
+        return (Math.random() * 1000) * randomChance;
+    }
 
     /**
      * Populates the HashMap with initial data.
@@ -45,9 +72,22 @@ public class Crash extends GamblingGame {
     private HashMap<User, Double> generateInitialPlayerMap(ArrayList<User> userList) {
         HashMap<User, Double> playerMap = new HashMap<>();
         for (User currUser : userList) {
-            playerMap.put(currUser, 0.0);
+            playerMap.put(currUser, 0.00);
         }
         return playerMap;
+    }
+
+    /**
+     * Populates the HashMap with initial data.
+     * @param userList The ArrayList of playing Users.
+     * @return HashMap that maps a User and an Integer.
+     */
+    private HashMap<User, Integer> generateInitialBetMap(ArrayList<User> userList) {
+        HashMap<User, Integer> betMap = new HashMap<>();
+        for (User currUser : userList) {
+            betMap.put(currUser, 0);
+        }
+        return betMap;
     }
 
     /**
@@ -55,8 +95,56 @@ public class Crash extends GamblingGame {
      */
     public void updatePlayerMultipliers() {
         for (User currUser : this.userManager.getCurrentActiveUsers()) {
-            if (userManager.getStatusOfUser(currUser).equals("Playing"))
-            currentPlayerMultipliers.replace(currUser, currentMultiplier);
+            if (userManager.getStatusOfUser(currUser).equals("Playing")) {
+                if (currentPlayerMultipliers.containsKey(currUser)) {
+                    currentPlayerMultipliers.replace(currUser, currentMultiplier);
+                } else {
+                    currentPlayerMultipliers.put(currUser, currentMultiplier);
+                }
+            }
+        }
+    }
+
+    /**
+     * Updates the User's bet amount.
+     * @param bet Integer that represents the bet amount.
+     * @param currUser Any User.
+     */
+    public void placeBet(int bet, User currUser){
+        userManager.updatePlayerBalance(currUser, currUser.getBalance() - bet);
+        if (currentPlayerBets.containsKey(currUser)) {
+            currentPlayerBets.replace(currUser, bet);
+        } else {
+            currentPlayerBets.put(currUser, bet);
+        }
+    }
+
+    /**
+     * Makes sure the game is running while the current multiplier is less than the busting multiplier.
+     */
+    private void updateGameStatus() {
+        gameRunning = this.currentMultiplier <= this.bustingMultiplier;
+    }
+
+    /**
+     * Toggles the game status.
+     */
+    public void toggleGame() { this.gameRunning = !this.gameRunning; }
+
+    public void updatePlayersBalance() {
+        for (User user : currentPlayerBets.keySet()) {
+            if (userManager.getStatusOfUser(user).equals("Won")) {
+                userManager.updatePlayerBalance(user, user.getBalance() + (int) Math.floor(currentPlayerBets.get(user) * currentPlayerMultipliers.get(user)));
+            }
+        }
+    }
+
+    public void setLosingPlayers() {
+        for (User user : userManager.getStatusOfUsers().keySet()) {
+            if (userManager.getStatusOfUser(user).equals("Playing") && !gameRunning) {
+                System.out.println("hit");
+                userManager.setStatusOfUser(user, "Lost");
+            }
         }
     }
 }
