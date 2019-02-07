5205d52ba66b37b142d3946c59d246c3e7671868
BT_1N3_33 <vli8558@bths.edu>
2019-02-07 09:05:31 -0500

    binrocksuper

--- src/Dependencies/Games/Coinflip.java
+++ src/Dependencies/Games/Coinflip.java
@@ -1,11 +1,14 @@
 package Dependencies.Games;
 
+import Dependencies.Systems.User;
+
 import java.awt.event.ActionEvent;
 
 public class Coinflip extends GamblingGame{
 
     public boolean heads = false;
     private double greenPos = 180;
+    private User u;
 
     public void bet(int b, double rotateAmnt)
     {
@@ -16,26 +19,17 @@ public class Coinflip extends GamblingGame{
         }
         if(heads && greenPos>=180)
         {
-
+            userManager.updatePlayerBalance(u,2*b);
+            System.out.println("You won " + 2*b);
         }
         if(!heads && greenPos<180)
         {
-
+            userManager.updatePlayerBalance(u,2*b);
+            System.out.println("You won " + 2*b);
+        }
+        else{
+            userManager.updatePlayerBalance(u,-b);
+            System.out.println("You lose " + -b);
         }
     }
-
-    public boolean roll(boolean head)
-    {
-        double roll = Math.random();
-        if(head && roll>.5)
-        {
-            return true;
-        }
-        if(!head && roll<.5)
-        {
-            return true;
-        }
-        return false;
-    }
-
 }
