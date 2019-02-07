2ca05d539c73f11886c57315730302bfe3895767
BT_1N3_32 <jhoang6606@bths.edu>
2019-02-07 10:12:59 -0500

    bent bi

--- src/Controllers/JackpotController.java
+++ src/Controllers/JackpotController.java
@@ -8,7 +8,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import  javafx.scene.control.TextField;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -22,6 +24,8 @@ public class JackpotController implements Initializable
     @FXML private Label timeNum;
     @FXML private Label winPercent;
     @FXML private Button submitButton;
+    @FXML private TextField field;
+    @FXML private Label prompt;
 
     private boolean gameState = false;
     private Jackpot jp;
@@ -33,6 +37,7 @@ public class JackpotController implements Initializable
         {
             gameState = true;
             long step = System.nanoTime() + 15000000000L;
+
             new AnimationTimer()
             {
                 public void handle(long now)
@@ -50,6 +55,31 @@ public class JackpotController implements Initializable
             }.start();
         }
     }
+
+    /**
+     * Makes sure the betting amount entered is a whole number that is less than 1,000,000,000.
+     * @return True if it meets the requirements, false otherwise.
+     */
+    private boolean validateBet() {
+        return (prompt.getText().length() < 10 && prompt.getText().length() > 0 && jp.userManager.validateAmount(LoginController.currentUser, Integer.parseInt(prompt.getText())));
+    }
+
+    private void displayAlert() {
+        if (field.getText().length() > 10) {
+            prompt.setText("Smaller Bet Pls.");
+        } else if (field.getText().matches(".")) {
+            prompt.setText("Whole # Only.");
+        } else if (field.getText().matches("[a-zA-Z]")){
+            prompt.setText("Numbers Pls.");
+        }
+    }
+
+    public void handler(javafx.event.ActionEvent e) {
+        if (!validateBet()) {
+            displayAlert();
+        }
+    }
+
     public void bet()
     {
 
